41a0eb99e5c0286b7fb95fe659b776cc63add9b1
Seow Ming Wei <50496970+mingweiseow@users.noreply.github.com>
2020-02-06 17:43:52 +0800

    Add files via upload

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,10 +92,10 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "contactform/contactform.php",
+      url: "mark@letsdutch.co",
       data: str,
       success: function(msg) {
-        // alert(msg);
+          alert(msg);
         if (msg == 'OK') {
           $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
